470a59ceac509bed5609af6b3c3801246c393ac5
LeandroRezendeCoutinho <leandro.rc.contato@gmail.com>
2019-07-03 10:36:55 -0300

    add atom install

---
+++ scripts/zypper/atom-install.sh
@@ -0,0 +1,11 @@
+if [ ! "`whoami`" = "root" ]
+then
+    echo "Use sudo to run this script"
+    exit 1
+fi
+
+sh -c 'echo -e "[Atom]\nname=Atom Editor\nbaseurl=https://packagecloud.io/AtomEditor/atom/el/7/\$basearch\nenabled=1\ntype=rpm-md\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://packagecloud.io/AtomEditor/atom/gpgkey" > /etc/zypp/repos.d/atom.repo'
+
+sudo zypper --gpg-auto-import-keys refresh
+
+sudo zypper --non-interactive install atom
