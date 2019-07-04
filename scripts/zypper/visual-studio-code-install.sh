265d880efd73f918124739a27c8b03fab94f5d6b
Leandro Rezende <leandrorezendecoutinho>
2019-07-04 10:25:17 -0300

    add visual studio code install

---
+++ scripts/zypper/visual-studio-code-install.sh
@@ -0,0 +1,13 @@
+if [ ! "`whoami`" = "root" ]
+then
+    echo "Use sudo to run this script"
+    exit 1
+fi
+
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/zypp/repos.d/vscode.repo'
+
+sudo zypper refresh
+
+sudo zypper --non-interactive install code
