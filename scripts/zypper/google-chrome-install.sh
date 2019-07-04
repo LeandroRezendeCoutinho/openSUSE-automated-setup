74fa4dcf3dcc5727813aaf93ff08afb11a096d86
Leandro Rezende <leandrorezendecoutinho>
2019-07-04 10:30:38 -0300

    add google chrome install

---
+++ scripts/zypper/google-chrome-install.sh
@@ -0,0 +1,11 @@
+if [ ! "`whoami`" = "root" ]
+then
+    echo "Use sudo to run this script"
+    exit 1
+fi
+
+zypper ar -f http://dl.google.com/linux/chrome/rpm/stable/x86_64/ Google
+
+rpm --import https://dl.google.com/linux/linux_signing_key.pub
+
+zypper --non-interactive install google-chrome-stable
