f456282e5e25b417fa129105655f541516e20238
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-07-06 10:44:37 -0300

    add docker install

--- zypper-install.sh
+++ zypper-install.sh
@@ -8,6 +8,7 @@ fi
 
 bash scripts/zypper/atom-install.sh
 bash scripts/zypper/devel-basis-install.sh
+bash scripts/zypper/docker-install.sh
 bash scripts/zypper/fish-install.sh
 bash scripts/zypper/google-chrome-install.sh
 bash scripts/zypper/node-install.sh
