45788388f102a693d2b8e0b5afd606a1702bf720
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-07-18 10:19:13 -0300

    change fish setup order

--- environment-setup.sh
+++ environment-setup.sh
@@ -1,5 +1,7 @@
 echo "Starting batch setup"
 
-bash scripts/setup/fish-setup.sh
 bash scripts/setup/git-setup.sh
 bash scripts/setup/tmux-setup.sh
+
+# fish need to be the last setup
+bash scripts/setup/fish-setup.sh
