7f406e5b7afa4627f6983cb0c6f740b9d393a66c
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2020-03-01 11:53:23 -0300

    update environment setup

--- environment-setup.sh
+++ environment-setup.sh
@@ -1,6 +1,8 @@
 echo "Starting batch setup"
 
+bash scripts/setup/docker-setup.sh
 bash scripts/setup/git-setup.sh
+bash scripts/setup/swappiness-setup.sh
 bash scripts/setup/tmux-setup.sh
 
 # fish need to be the last setup
