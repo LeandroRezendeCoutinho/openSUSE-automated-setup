c13cb03f50c5f86f703f4697efecb6cfbad5fe57
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2020-12-04 10:53:16 -0300

    update zsh setup

--- environment-setup.sh
+++ environment-setup.sh
@@ -1,9 +1,11 @@
 echo "Starting batch setup"
 
 bash scripts/setup/docker-setup.sh
+bash scripts/setup/flatpak-setup.sh
 bash scripts/setup/git-setup.sh
 bash scripts/setup/swappiness-setup.sh
 bash scripts/setup/tmux-setup.sh
+bash scripts/setup/zsh-setup.sh
 
 # disabled
 # bash scripts/setup/fish-setup.sh
--- scripts/setup/zsh-setup.sh
+++ scripts/setup/zsh-setup.sh
@@ -7,7 +7,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 # Oh My Zsh theme spaceship-prompt
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+sudo ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
 # default config
 cp ../config/.zshrc ~/
