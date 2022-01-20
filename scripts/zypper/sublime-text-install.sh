572a94de972c5c52ca92b940ab3cea5296655835
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2022-01-20 16:30:40 -0300

    update sublime text install

--- scripts/zypper/sublime-text-install.sh
+++ scripts/zypper/sublime-text-install.sh
@@ -6,6 +6,8 @@ then
     exit 1
 fi
 
+rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+
 zypper addrepo -g -f https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
 
 zypper --non-interactive install sublime-text
